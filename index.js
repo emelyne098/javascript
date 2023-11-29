b383f570f48fa524e6ff8f568b8cdb5f55eee2be
IRADUKUNDA <iradukundemelyne09@gmail.com>
2023-11-29 14:26:44 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+let isitcountry=true;
+console.log(typeof isitcountry);
+let small=164758757654674856256732n;
+console.log(typeof small);
+let year=2004;
+console.log(typeof year);
+let b=1/0;
+console.log(typeof b);
+console.log(b);
+let country ='Rwanda';
+console.log(typeof country)
+console.log(country);
+let myvariable=null;
+console.log(myvariable);
+console.log(typeof myvariable);
+function Person(){
+    this.firstName='ira',
+    this.lastName='emln'
+}
+const Person1 = new Person();
+const Person2 = new Person();
+console.log(Person1);
+console.log(Person2);
